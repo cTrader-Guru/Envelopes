f1614d039e013e4d4ca1018286826098e2882185
Leonardo <leonardo.ciaccio@gmail.com>
2022-06-22 12:54:36 +0200

    Aggiungere i file di progetto.

---
+++ Envelopes/Envelopes.cs
@@ -0,0 +1,52 @@
+﻿using cAlgo.API;
+using cAlgo.API.Indicators;
+
+namespace cAlgo
+{
+
+
+    [Indicator(IsOverlay = true, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class Envelopes : Indicator
+    {
+
+
+        [Parameter(DefaultValue = 14)]
+        public int Period { get; set; }
+
+        [Parameter(DefaultValue = 0.1)]
+        public double Deviation { get; set; }
+
+        [Parameter("MA Type", DefaultValue = MovingAverageType.Simple)]
+        public MovingAverageType MAType { get; set; }
+
+        [Output("Upper Band", LineColor = "#B268BCFF")]
+        public IndicatorDataSeries UpperBand { get; set; }
+
+        [Output("Main", LineColor = "#AA3F3F3F")]
+        public IndicatorDataSeries Main { get; set; }
+
+        [Output("Lower Band", LineColor = "#B2FF5861")]
+        public IndicatorDataSeries LowerBand { get; set; }
+
+        private MovingAverage _movingAverage;
+
+        protected override void Initialize()
+        {
+
+            _movingAverage = Indicators.MovingAverage(Bars.ClosePrices, Period, MAType);
+
+        }
+
+        public override void Calculate(int index)
+        {
+            var maValue = _movingAverage.Result[index];
+
+            Main[index] = maValue;
+            UpperBand[index] = maValue * (1 + Deviation / 100);
+            LowerBand[index] = maValue * (1 - Deviation / 100);
+
+        }
+
+    }
+
+}
